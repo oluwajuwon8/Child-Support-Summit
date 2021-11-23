08a2f7639c0b91cf26c8c8e5e9ac997e80410d9f
oluwajuwon8 <azeezatlola@gmail.com>
2021-11-23 11:57:32 +0100

    Add speakers

--- script.js
+++ script.js
@@ -15,6 +15,30 @@ const speakerDetails = [{
   speakerProfile: 'mental health advocate and speaker',
   profileDetails: 'She is a bestselling parenting author, CBC Radio parenting columnist, and mental health advocate and speaker. In 2014, she teamed up with Todays Parent magazine to make this series of 60-second parenting tips videos.',
 },
+{
+  speakerImg: 'images/speaker3.jpg',
+  speakerName: 'Victoria Adejoke Orelope-Adefulire',
+  speakerProfile: 'Commissioner for Women Affairs and Poverty Alleviation ',
+  profileDetails: 'She was the former Deputy-Governor of Lagos State from 2011 to 2015.[2] Prior to that she was the Commissioner for Women Affairs and Poverty Alleviation 2003-2011.',
+},
+{
+  speakerImg: 'images/speaker6.jpg',
+  speakerName: 'Kamala Devi Harris',
+  speakerProfile: 'American politician and attorney',
+  profileDetails: 'American politician and attorney who is the 49th and current vice president of the United States. She is the first female vice president and the highest-ranking female official in U.S. history, as well as the first African American and first Asian American vice president.',
+},
+{
+  speakerImg: 'images/speaker5.jpg',
+  speakerName: 'Barack Hussein Obama II ',
+  speakerProfile: 'American politician, author, and retired attorney',
+  profileDetails: 'An American politician, author, and retired attorney who served as the 44th president of the United States from 2009 to 2017. A member of the Democratic Party, Obama was the first African-American president of the United States.',
+},
+{
+  speakerImg: 'images/femi-falana.jpg',
+  speakerName: 'Femi Falana',
+  speakerProfile: 'Activist for human right',
+  profileDetails: 'Femi Falana is a well activist for human right, always trying to show sympathy towards people,Femi Falana is well known for opposing opression, from successive military authorities.',
+},
 ];
 
 const speakers = () => {
